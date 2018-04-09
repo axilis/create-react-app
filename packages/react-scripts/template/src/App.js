88014c2cad9cc675549a4749e2019100248e063e
Vjeran Hanžek <vjeran.hanzek@axilis.com>
2018-04-09 15:00:01 +0200

    Updated React template files

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-import { hot } from 'react-hot-loader';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './modules/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
     );
   }
 }
 
-export default hot(module)(App);
+export default App;
--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import * as serviceWorker from './serviceWorker';
+import Root from './Root';
+import './index.css';
+
+ReactDOM.render(<Root />, document.getElementById('root'));
