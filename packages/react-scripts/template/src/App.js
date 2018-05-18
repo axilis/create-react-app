0c391cd18a0fdcb3021ff9049c97e459419c53be
Vjeran Hanžek <vjeran.hanzek@axilis.com>
2018-05-18 15:57:03 +0200

    Added sentry support to react-scripts

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter as Router } from 'react-router-redux';
 import { hot } from 'react-hot-loader';
+import Raven from 'raven-js';
 
 import store from './config/redux/store';
 import history from './config/router/history';
 import Layout from './Layout';
 
 class App extends Component {
+  componentDidCatch(error, errorInfo) {
+    Raven.captureException(error, { extra: errorInfo });
+  }
+
   render() {
     return (
       <Provider store={store}>
--- packages/react-scripts/template/src/config/redux/store.js
+++ packages/react-scripts/template/src/config/redux/store.js
@@ -1,11 +1,18 @@
+import Raven from 'raven-js';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { routerMiddleware } from 'react-router-redux';
+import createRavenMiddleware from "raven-for-redux";
 
 import reducer from './reducer';
 import history from '../router/history';
 
 export default createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(routerMiddleware(history)))
+  composeWithDevTools(
+    applyMiddleware(
+      routerMiddleware(history),
+      createRavenMiddleware(Raven),
+    )
+  )
 );
